d1bb44ee31b4ab4f2b61fea0775ca2b147658818
Pierre SZLEPER <psz@ubuntu.com>
2022-08-20 00:13:32 +0200

    Created two utils functions:   - ft_malloc(size_t x) allocates x bytes of memory and sets them all to \0     If theres is a malloc error, it prints the error and exits with errno   - ft_trim_whitespace(char *str) removes leading and trailing whitespace from str   "   exit      43         " -> "exit      43"

--- utils.c
+++ utils.c
@@ -6,11 +6,53 @@
 /*   By: pszleper <pszleper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 20:35:22 by pszleper          #+#    #+#             */
-/*   Updated: 2022/08/17 19:02:19 by pszleper         ###   ########.fr       */
+/*   Updated: 2022/08/20 00:06:13 by pszleper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+/*
+  returns num_bytes amount of allocated memory, all set to NULl
+  in case of malloc error prints the error and exits with errno code
+*/
+char	*ft_malloc(size_t num_bytes)
+{
+	char	*allocated;
+	int		i;
+
+	i = 0;
+	allocated = malloc(num_bytes);
+	if (allocated == NULL)
+	{
+		perror(NULL);
+		exit(errno);
+	}
+	while (allocated[i])
+	{
+		allocated[i] = '\0';
+		i++;
+	}
+	return (allocated);
+}
+
+/*
+  Trims starting and trailing whitespace of str
+  "   exit    43  	  " produces "exit    43"
+*/
 char	*ft_trim_whitespace(char *str)
 {
-	return (str);
+	unsigned int	start;
+	size_t			end;
+	char			*trimmed;
+
+	start = 0;
+	end = ft_strlen(str) - 1;
+	trimmed = ft_malloc(ft_strlen(str));
+	while (str[start] && ft_is_space(str[start]))
+		start++;
+	while (str[end] && ft_is_space(str[end]))
+		end--;
+	trimmed = ft_substr(str, start, end - start + 1);
+	return (trimmed);
 }
