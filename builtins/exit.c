1041d1e259f1fbaf127f5e2984ce75ccf7b82934
Pierre SZLEPER <psz@ubuntu.com>
2022-08-19 22:27:04 +0200

    Modified ft_exit to exit with 'status' code

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,15 +6,20 @@
 /*   By: pszleper <pszleper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 21:20:32 by pszleper          #+#    #+#             */
-/*   Updated: 2022/08/19 21:41:20 by pszleper         ###   ########.fr       */
+/*   Updated: 2022/08/19 22:26:33 by pszleper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	ft_exit(char *input)
+/*
+  Clears the shell's history
+  Frees input buffer
+  Exits the shell with 'status' code
+*/
+void	ft_exit(int status, char *input)
 {
 	rl_clear_history();
 	ft_free((void **) &input);
-	exit(0);
+	exit(status);
 }
