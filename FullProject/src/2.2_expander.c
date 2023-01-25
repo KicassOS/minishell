49123a02fb4728968f3e2f465cc4b07f57b34c06
Inaara Azimzhanova <43515616+Inaara@users.noreply.github.com>
2023-01-25 04:37:13 +0100

    now expands e.g echo "${USER}" but idk

--- FullProject/src/2.2_expander.c
+++ FullProject/src/2.2_expander.c
@@ -12,22 +12,56 @@
 
 #include "minishell.h"
 
+static int	static_ft_has_closing_quote(char **str, int i)
+{
+	while ((*str)[i + 2] != '\0')
+	{
+		(*str)[i + 1] = (*str)[i + 2];
+		i++;
+		if ((*str)[i] == ' ')
+			return(EXIT_FAILURE);
+		if ((*str)[i] == '}')
+			return(EXIT_SUCCESS);
+	}
+	return(EXIT_FAILURE);
+}
+
+static void	static_ft_resolve_quote_case(char **token, t_index *index,
+		t_data *data, t_slist **parts)
+{
+	if (!(static_ft_has_closing_quote(token, index->i)))
+	{
+		index->i = index->i + ft_expand_var(token,
+			index->i, parts, data->myenv) + 2;
+		index->pos = index->i;
+	}
+	else
+	{
+//		ft_printf_stderr("%s: bad sunstitution\n", SHELL);
+		ft_exit_expander(parts, token, data->myenv);
+	}
+}
+
 static int	static_ft_trim_two(char **str, int i)
 {
 	int	count;
+	int	c;
 
 	count = 2;
+	c = 0;
 	while ((*str)[i + count] != '\0')
 	{
+		c++;
 		(*str)[i] = (*str)[i + count];
 		i++;
 	}
 	while ((*str)[i] != '\0')
 	{
+		c++;
 		(*str)[i] = '\0';
 		i++;
 	}
-	return (0);
+	return (c - 3);
 }
 
 static int	static_ft_fetchexitcode(t_slist **list,	int lastExitStatus)
@@ -67,6 +101,8 @@ void	ft_determine_expansion_type(char **token, t_index *index,
 				index->i, parts, data->myenv);
 		index->pos = index->i;
 	}
+	else if ((*token)[index->i + 1] == '{')
+		static_ft_resolve_quote_case(token, index, data, parts);
 	else
 		index->i = index->i + 1;
 }
