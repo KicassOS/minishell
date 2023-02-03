2bedd93f2e8b7f02ed5cdcf4481c36b7a8e18610
Inaara AZIMZHANOVA <iazimzha@e2r12p23.clusters.42paris.fr>
2023-02-03 19:13:03 +0100

    Echo builting -n fixed

--- src/echo.c
+++ src/echo.c
@@ -6,7 +6,7 @@
 /*   By: iazimzha <iazimzha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 13:53:44 by iazimzha          #+#    #+#             */
-/*   Updated: 2023/02/03 18:06:14 by iazimzha         ###   ########.fr       */
+/*   Updated: 2023/02/03 19:11:40 by iazimzha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static void	ft_check_newlineflag(char **args, int *print_newline, int *i)
 {
-	if (ft_strlen(args[0]) == 2 && ft_strnstr(args[0], "-n", 2))
+	if (args[1] && ft_memcmp(args[1], "-n\0", 3) == 0)
 	{
 		*print_newline = 0;
 		*i += 1;
@@ -26,12 +26,10 @@ int	ft_echo(char **args)
 	int	print_newline;
 	int	i;
 
-	if (*args)
-		args++;
 	if (*args == NULL)
 		return (EXIT_SUCCESS);
 	print_newline = 1;
-	i = 0;
+	i = 1;
 	if (!args)
 	{
 		ft_free_args(args);
